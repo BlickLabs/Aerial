ecb1360ee620fdcde08f9fcb10962e3c6ce79300
MitchelHdz <sebastianvicky_@hotmail.com>
2016-05-18 20:39:32 -0500

    Fixing responsive corrections and portfolio bugs, background video added

--- api.php
+++ api.php
@@ -4,13 +4,11 @@ include "config.php";
 //header("Content-Type: application/json; charset=UTF-8");
 error_reporting(E_ALL);
 
-$category =mysqli_real_escape_string($mysqli1,$_GET["category"]);
+$category =mysqli_real_escape_string($mysqli3,$_GET["category"]);
 $drone =mysqli_real_escape_string($mysqli2,$_GET["drone"]);
 
-
-
 //query only by Party Room (ID)
-if ($category!=null && empty($drone)) {
+if ($category!=null && $drone!=null){
         
         $result = $mysqli->query("SELECT id_category,name_category FROM categorys WHERE id_category = '" . $category . "'");
         while ($row = $result->fetch_array(MYSQLI_ASSOC)) {
@@ -22,7 +20,7 @@ if ($category!=null && empty($drone)) {
             );
         }
 
-        $result2 = $mysqli2->query("SELECT c.tittle,c.route,g.name_category FROM content_dron_category AS cdc LEFT JOIN content AS c ON c.id_content = cdc.id_content LEFT JOIN categorys AS g ON g.id_category = cdc.id_category WHERE cdc.id_category ='" . $category ."'");
+        $result2 = $mysqli2->query("SELECT c.tittle,c.route,g.name_category FROM content_dron_category AS cdc LEFT JOIN content AS c ON c.id_content = cdc.id_content LEFT JOIN categorys AS g ON g.id_category = cdc.id_category WHERE cdc.id_category ='" . $category ."' AND cdc.id_drone ='". $drone ."'");
         while ($row = $result2->fetch_array(MYSQLI_ASSOC)) {
             $description = mysqli_real_escape_string($mysqli2,$row['description']);
             $path=mysqli_real_escape_string($mysqli2,'php/album/' . $row['route']);
