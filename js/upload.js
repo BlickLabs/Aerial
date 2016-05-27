8937e68eb189626318d738ae0accaf1922a7aeec
MitchelHdz <sebastianvicky_@hotmail.com>
2016-05-27 14:18:13 -0500

    Upload.js re-indented

--- js/upload.js
+++ js/upload.js
@@ -1,25 +1,14 @@
 $(function () {
     $('#subida').submit(function () {
-
-       var comprobar = $('#foto').val().length*$('#drone').val().length*$('#category').val().length*$('#creation_date').val().length;	
-        
-        
+        var comprobar = $('#foto').val().length*$('#drone').val().length*$('#category').val().length*$('#creation_date').val().length;	
         if (comprobar > 0) {
-
             var formulario = $('#subida');
-
             var datos = formulario.serialize();
-
             var archivos = new FormData();
-           
             var url = 'php/Upload_Photo.php';
-
             for (var i = 0; i < (formulario.find('input[type=file]').length); i++) {
-
                 archivos.append((formulario.find('input[type="file"]:eq(' + i + ')').attr("name")), ((formulario.find('input[type="file"]:eq(' + i + ')')[0]).files[0]));
-
             }
-
             $.ajax({
                 url: url + '?' + datos,
                 type: 'POST',
@@ -27,65 +16,34 @@ $(function () {
                 data: archivos,
                 processData: false,
                 beforeSend: function () {
-
                     $('#cargando').show(300);
-
                 },
                 success: function (data) {
-
                     $('#cargando').hide(900);
                     $(location).attr('href', 'images.php');
-                   
-
-                    return false;
-                }
-
-            });
-
+                        return false;
+                    }
+                });
             return false;
-
-        } else {
-
-
+            } 
+        else {
             var imagen = document.getElementById("foto").files;
-            if (imagen.length == 0)
-
-            {
-              //  $('#foto').after('<div class="alert alert-danger">No has seleccionado ningun archivo</div>');
-                
+            if (imagen.length == 0){
                 bootbox.alert("No has seleccionado ningun archivo");
                 return false;
             }
-            else
-
-            {
-
-                for (x = 0; x < imagen.length; x++)
-
-                {
-
-
-
-                    if (imagen[x].type != "image/png" && imagen[x].type != "image/jpg" && imagen[x].type != "image/jpeg" && imagen[x].type != "image/gif")
-
-                    {
-                        //$('#foto').after('<div class="alert alert-danger">El archivo ' + imagen[x].name + '  no es una imagen, selecciona una imagen</div>');
+            else{
+                for (x = 0; x < imagen.length; x++){
+                    if (imagen[x].type != "image/png" && imagen[x].type != "image/jpg" && imagen[x].type != "image/jpeg" && imagen[x].type != "image/gif"){
                         bootbox.alert("El archivo" + imagen[x].name+ " no es una imagen");    
                         return false;
-
                     }
-
-
-                    if (imagen[x].size > 1024 * 1024 * 2)
-
-                    {
-                        //$('#foto').after('<div class="alert alert-danger">La imagen ' + imagen[x].name + '  pesa mas de 3 MB selescciona otro archivo</div>');
+                    if (imagen[x].size > 1024 * 1024 * 2){
                         bootbox.alert("El archivo   " + imagen[x].name+ " sobrepasa el peso permitido");    
                         return  false;
                     }
                 }
             }
-
         }
     });
 });
