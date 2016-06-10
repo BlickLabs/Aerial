96fde13fe202281c17fa937cf5e58529e18b69c0
Mauricio Ivan Mejia Ramirez <mauriciodinki@gmail.com>
2016-06-09 22:38:04 -0500

    Changed php script to upload a photo

--- php/Upload_Photo.php
+++ php/Upload_Photo.php
@@ -1,25 +1,31 @@
 <?php
     include "../config.php";
     error_reporting(E_ALL);
-    //query para insertar imagen obtenemos valores por get los cuales los recibimos de query por post
     $title = $_GET['title'];
-    $desc = $_GET['description'];
-    $st = $_GET['status'];
-    $cd = $_GET['creation_date'];
+    $drone = $_GET['drone'];
+    $category = $_GET['category'];
+    $creation_date = $_GET['creation_date'];
     $foto = trim($_FILES['foto']['name']);
-    $ingresar = mysqli_query($mysqli3, "INSERT INTO content (tittle,route,creation_date)"
-                             . "VALUES('$title','$foto','$cd')");
-    move_uploaded_file($_FILES['foto']['tmp_name'], 'album/' . $foto);
-    $id_img = mysqli_insert_id($mysqli3); //obtenemos el id del ultimo insert realizado
-    $mysqli3->close(); //cerramos la conexió del primer query
-    $id_drone=$_GET['drone'];
-    $id_category=$_GET['category'];
-    //query para relacionar imagen con galeria(s) 
-   
-            mysqli_query($mysqli2,"INSERT INTO content_dron_category (id_content,id_drone,id_category) 
-                VALUES ('".$id_img."','".$id_drone."','".$id_category."')");
-           
-            $mysqli2->close(); 
-       
-        
+    $filename = "";
+    if ($_FILES['foto']['size'] <= 2097152) {
+        while (true) {
+            $filename = uniqid(rand()) . '.' .pathinfo($foto, PATHINFO_EXTENSION);
+            if (!file_exists('album/' . $filename)) break;
+        }
+        error_log($filename, 0);
+        error_log("Despues de cambiar el nombre", 0);
+        // Guardamos la imagen (titulo, archivo, fecha de creacion)
+        $query1 = "INSERT INTO Images (title, file, category, drone, creation_date) VALUES ('$title', '$filename', '$category', '$drone', '$creation_date')";
+        mysqli_query($mysqli, $query1);
+        error_log("Despues del primer insert", 0);
+        // Movemos el archivo
+        move_uploaded_file($_FILES['foto']['tmp_name'], 'album/' . $filename);
+        error_log("Despues del mover el archivo", 0);
+        //Cerramos la conexion
+        $mysqli->close();
+    } else {
+        header('HTTP/1.1 500 Internal Server Error');
+        header('Content-Type: application/json; charset=UTF-8');
+        die(json_encode(array('message' => 'ERROR', 'code' => 1337)));
+    }
 ?>
