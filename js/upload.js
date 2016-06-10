be52ad32772951a3ea93d4430de520d974ea4357
Mauricio Ivan Mejia Ramirez <mauriciodinki@gmail.com>
2016-06-09 22:36:42 -0500

    #27, Refactor upload script

--- js/upload.js
+++ js/upload.js
@@ -1,48 +1,53 @@
-$(function () {
-    $('#subida').submit(function () {
-        var comprobar = $('#foto').val().length*$('#drone').val().length*$('#category').val().length*$('#creation_date').val().length;	
-        if (comprobar > 0) {
-            var formulario = $('#subida');
-            var datos = formulario.serialize();
-            var archivos = new FormData();
-            var url = 'php/Upload_Photo.php';
-            for (var i = 0; i < (formulario.find('input[type=file]').length); i++) {
-                archivos.append((formulario.find('input[type="file"]:eq(' + i + ')').attr("name")), ((formulario.find('input[type="file"]:eq(' + i + ')')[0]).files[0]));
-            }
-            $.ajax({
-                url: url + '?' + datos,
-                type: 'POST',
-                contentType: false,
-                data: archivos,
-                processData: false,
-                beforeSend: function () {
-                    $('#cargando').show(300);
-                },
-                success: function (data) {
-                    $('#cargando').hide(900);
-                    $(location).attr('href', 'images.php');
-                        return false;
-                    }
-                });
-            return false;
-            } 
-        else {
-            var imagen = document.getElementById("foto").files;
-            console.log(imagen);
-            if (imagen.length == 0){
-                bootbox.alert("No has seleccionado ningun archivo");
-                return false;
-            }
-            alert(imagen[0].size);
-            console.log(imagen[0]);
-            if (imagen[0].type != "image/png" && imagen[0].type != "image/jpg" && imagen[0].type != "image/jpeg" && imagen[0].type != "image/gif"){
-                bootbox.alert("El archivo" + imagen[0].name+ " no es una imagen");    
-                return false;
-            }
-            if (imagen[0].size > 1024 * 1024 * 2){
-                bootbox.alert("El archivo   " + imagen[0].name+ " sobrepasa el peso permitido");    
-                return  false;
-            }
+$(function() {
+  $('#subida').submit(function() {
+    var comprobar = $('#foto').val().length * $('#drone').val().length * $('#category').val().length * $('#creation_date').val().length;
+    if (comprobar > 0) {
+      var formulario = $('#subida');
+      var datos = formulario.serialize();
+      var archivos = new FormData();
+      var url = 'php/Upload_Photo.php';
+      for (var i = 0; i < (formulario.find('input[type=file]').length); i++) {
+        archivos.append((formulario.find('input[type="file"]:eq(' + i + ')').attr("name")), ((formulario.find('input[type="file"]:eq(' + i + ')')[0]).files[0]));
+      }
+      console.log("Antes del ajax");
+      $.ajax({
+        url: url + '?' + datos,
+        type: 'POST',
+        contentType: false,
+        data: archivos,
+        processData: false,
+        beforeSend: function() {
+          $('#cargando').modal('show');
+        },
+        success: function (data) {
+            $('#cargando h3').text('Imagen subida correctamente.');
+            setTimeout(function () {
+                $(location).attr('href', 'create_file.php');
+            }, 2000);
+        },
+        error: function(data) {
+            $('#cargando h3').text('Ocurrio un error. Intentalo de nuevo.');
         }
-    });
+      });
+      return false;
+    } else {
+      var imagen = document.getElementById("foto").files;
+      if (imagen.length === 0) {
+        bootbox.alert("No has seleccionado ningun archivo");
+        return false;
+      } else {
+        var imagen = document.getElementById("foto").files;
+        for (x = 0; x < imagen.length; x++) {
+          if (imagen[x].type != "image/png" && imagen[x].type != "image/jpg" && imagen[x].type != "image/jpeg") {
+            bootbox.alert("El archivo" + imagen[x].name + " no es una imagen");
+            return false;
+          }
+          if (imagen[x].size > 1024 * 1024 * 2) {
+            bootbox.alert("El archivo   " + imagen[x].name + " sobrepasa el peso permitido");
+            return false;
+          }
+        }
+      }
+    }
+  });
 });
